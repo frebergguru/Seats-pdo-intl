47ebd7e1e7365e8f2ed959e567776346aa7f9422
Hypnotize <morten@freberg.guru>
2023-01-30 00:08:30 +0100

    Updated README.md and renamed one of the sql files.

--- README.md
+++ README.md
@@ -8,3 +8,41 @@ A simple seat booking system with support for multiple languages.
 * Remove all unnecessary code
 * Optimize the code
 * Add more features like change seat reservation, delete account and so on
+
+# MySQL:
+**Example:**
+
+1. **Create a MySQL user:**
+```mysql
+CREATE USER 'lanparty'@'localhost' IDENTIFIED BY 'password';
+```
+
+2. **Create and import the MySQL database with:**
+```shell
+mysql -u lanparty -p < Seats-MySQL.sql
+```
+3. **Grant the user access to the database:**
+```mysql
+GRANT SELECT, INSERT, UPDATE, DELETE ON lanparty.* TO 'lanparty'@'localhost';
+FLUSH PRIVILEGES;
+```
+
+# PostgreSQL:
+**Example**
+
+1. **Create the PostgreSQL user:**
+```pgsql
+CREATE USER username WITH LOGIN PASSWORD 'password';
+```
+
+2. **Create and mport the database from the SQL:**
+```shell
+psql -U username -d database_name < Seats-PostgreSQL.sql
+```
+
+3. Give the database user access to the database and to the sequences user_id_seq and reservations_id_seq:**
+```pgsql
+GRANT CONNECT ON DATABASE lanparty TO lanparty;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO lanparty;
+GRANT USAGE, SELECT ON SEQUENCE users_id_seq, reservations_id_seq TO lanparty;
+```
---
+++ Seats-MySQL.sql
@@ -0,0 +1,93 @@
+--
+-- Create the database lanparty if it not exists
+--
+
+CREATE DATABASE IF NOT EXISTS lanparty;
+
+--
+-- Table structure for table `config`
+--
+
+DROP TABLE IF EXISTS `config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL,
+  `maxseats` int(11) NOT NULL,
+  `seat_width` int(11) NOT NULL,
+  `seat_height` int(11) NOT NULL,
+  `width` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `config`
+--
+
+LOCK TABLES `config` WRITE;
+/*!40000 ALTER TABLE `config` DISABLE KEYS */;
+INSERT INTO `config` VALUES (0,500,15,15,20);
+/*!40000 ALTER TABLE `config` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `taken` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` text NOT NULL,
+  `nickname` text NOT NULL,
+  `password` text NOT NULL,
+  `email` text NOT NULL,
+  `forgottoken` char(64) DEFAULT NULL,
+  `rseat` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
